03a96d7186744f80998678b8a3c3f85a77f84a6d
Maxence Petitpierre <maxence.petitpierre@vadesecure.com>
2021-12-20 09:53:07 +0100

    Use producer package Message

--- consumer.go
+++ consumer.go
@@ -8,16 +8,11 @@ import (
 	"syscall"
 	"time"
 
-	bassoon "github.com/MaxencePPierre/effective-bassoon"
+	bassoon "github.com/MaxencePPierre/effective-bassoon/message"
 	"github.com/nsqio/go-nsq"
 )
 
 type messageHandler struct{}
-type Message struct {
-	Name      string
-	Content   string
-	Timestamp string
-}
 
 const (
 	localhost string = "127.0.0.1"
@@ -27,6 +22,7 @@ const (
 func main() {
 	//The only valid way to instantiate the Config
 	config := nsq.NewConfig()
+
 	//Tweak several common setup in config
 	// Maximum number of times this consumer will attempt to process a message before giving up
 	config.MaxAttempts = 10
@@ -37,15 +33,18 @@ func main() {
 	config.DefaultRequeueDelay = time.Second * 0
 	//Init topic name and channel
 	channel := "Channel_Example"
+
 	//Creating the consumer
 	consumer, err := nsq.NewConsumer(bassoon.Topic, channel, config)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	// Set the Handler for messages received by this Consumer.
 	consumer.AddHandler(&messageHandler{})
 	//Use nsqlookupd to find nsqd instances
-	consumer.ConnectToNSQLookupd(localhost+":"+port)
+	consumer.ConnectToNSQLookupd(localhost + ":" + port)
+
 	// wait for signal to exit
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
@@ -56,14 +55,12 @@ func main() {
 
 // HandleMessage implements the Handler interface.
 func (h *messageHandler) HandleMessage(m *nsq.Message) error {
-	//Process the Message
 	var request bassoon.Message
 	if err := json.Unmarshal(m.Body, &request); err != nil {
 		log.Println("Error when Unmarshaling the message body, Err : ", err)
-		// Returning a non-nil error will automatically send a REQ command to NSQ to re-queue the message.
 		return err
 	}
-	//Print the Message
+
 	log.Println("Message")
 	log.Println("--------------------")
 	log.Println("Name : ", request.Name)
@@ -71,6 +68,7 @@ func (h *messageHandler) HandleMessage(m *nsq.Message) error {
 	log.Println("Timestamp : ", request.Timestamp)
 	log.Println("--------------------")
 	log.Println("")
+
 	// Will automatically set the message as finish
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/MaxencePPierre/physic-guide
 
 go 1.17
+
+require (
+	github.com/MaxencePPierre/effective-bassoon v0.0.0-20211219210744-a9e47cfcc25b
+	github.com/nsqio/go-nsq v1.1.0
+)
+
+require (
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/rs/zerolog v1.26.1 // indirect
+)
